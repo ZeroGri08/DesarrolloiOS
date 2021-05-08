f534d6cb4df4f5ae17171be398dd89b63bed1e30
ZeroGri08 <55572671+ZeroGri08@users.noreply.github.com>
2021-05-08 09:47:19 -0500

    Practica fotos parte 1

---
+++ galeriaCollectionView/galeriaCollectionView/fotosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  fotosCollectionViewCell.swift
+//  galeriaCollectionView
+//
+//  Created by Antonio Hernández on 08/05/21.
+//
+
+import UIKit
+
+class fotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var foto: UIImageView!
+}
